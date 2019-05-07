4c9bdbe5f9c6cd1e35865c9716c85b44a6539b18
Rodrigo Borges <rodrigo_craft@live.com>
2019-05-07 15:24:46 -0300

    otimização e função para criar textura - v2

--- Assets/Scripts/Tests.cs
+++ Assets/Scripts/Tests.cs
@@ -4,40 +4,57 @@ using UnityEngine;
 
 public class Tests : MonoBehaviour {
 
-	void Start () {
+    float ct = 0;
+    const int max = 100;
+    int a, b, c;
+    int pa, pb, pc;
+    Texture2D t1;
+    Texture2D t2;
+    Texture2D t3;
+
+    Texture2D GenerateTexture(Color c)
+    {
+        int size = 64;
+        Texture2D t = new Texture2D(size, size, TextureFormat.RGBA32, true, true);
+        for(int i = 0; i < size; i++)
+        {
+            for(int j = 0; j < size; j++)
+                t.SetPixel(i, j, c);
+        }
+        t.Apply();
+        return t;
+    }
+
+    void Start()
+    {
         FileManagement fm = new FileManagement();
         /*List<string> info = new List<string>();
         info.Add("LOG LINE 1");
         info.Add("LOG LINE 2");
         fm.WriteLog(info);*/
-        List<string> r = fm.ReadFile("entrada.txt");
+
+        /*List<string> r = fm.ReadFile("entrada.txt");
         foreach(string s in r)
         {
             print(s);
-        }
+        }*/
 
+        t1 = GenerateTexture(Color.red);
+        t2 = GenerateTexture(Color.green);
+        t3 = GenerateTexture(Color.blue);
     }
 
-    float ct = 0;
-    const int max = 100;
-    int a, b, c;
-    int pa, pb, pc;
-
-    private void OnGUI()
+    void Update()
     {
         ct += Time.deltaTime;
-        if(ct > 0.5)
+        if (ct > (1/30))
         {
             a += Random.Range(0, 10);
             b += Random.Range(0, 10);
             c += Random.Range(0, 10);
             ct = 0;
         }
-        GUI.Box(new Rect(10, 0, 200, 20), string.Format("PA: {0} | PB: {1} | PC: {2}", pa, pb, pc));
-        GUI.Box(new Rect(a, 30, 64, 64), "(PA)" + a);
-        GUI.Box(new Rect(b, 104, 64, 64), "(PB)" + b);
-        GUI.Box(new Rect(c, 178, 64, 64), "(PC)" + c);
-        
+
         if (a > max)
         {
             a = 0;
@@ -53,6 +70,13 @@ public class Tests : MonoBehaviour {
             c = 0;
             ++pc;
         }
+    }
 
+    void OnGUI()
+    {
+        GUI.Box(new Rect(10, 0, 200, 20), string.Format("PA: {0} | PB: {1} | PC: {2}", pa, pb, pc));
+        GUI.Box(new Rect(a, 30, 64, 64), t1);
+        GUI.Box(new Rect(b, 104, 64, 64), t2);
+        GUI.Box(new Rect(c, 178, 64, 64), t3);
     }
 }
