a7dcc3cbf3cad19c36b8991eb79d251eb50a962a
Rodrigo Borges <rodrigo_craft@live.com>
2019-06-07 13:25:09 -0300

    Add files via upload

---
+++ Assets/Scripts/Algorithms.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Algorithms
+{
+    public static Process InExecution = null;
+
+    public static List<Process> SJF(List<Process> processes, float executionTime)
+    {
+        List<Process> aux = new List<Process>();
+
+        for(int i = 0; i < processes.Count; i++)
+        {
+            if(processes[i].GetIncoming() <= executionTime)
+                aux.Add(processes[i]);
+        }
+
+        processes.RemoveAt(0);
+        processes.Sort((a, b) => {
+            return a.GetExecution().CompareTo(b.GetExecution());
+        });
+        processes.Insert(0, InExecution);
+        return processes;
+    }
+}
